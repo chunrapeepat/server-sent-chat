c06d5fb3ed5db79b6b753c2948f0303fb8edd187
chunza2542 <hello@thechun.dev>
2021-02-20 11:13:15 +0700

    Add connect endpoint for incoming connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,11 +13,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Define endpoints
+app.post("/connect", (req: Request, res: Response) => {
+  const { user } = req.body;
+  if (!user) {
+    return res.status(400).json({ error: true, message: "invalid payload" });
+  }
+  // TODO: boardcast message to all client
+  return res.status(200).json({ success: true });
+});
+
 app.post("/message", (req: Request, res: Response) => {
   const { user, message } = req.body;
   if (!user || !message) {
     return res.status(400).json({ error: true, message: "invalid payload" });
   }
+
   // TODO: boardcast message to all client
   return res.status(200).json({ success: true });
 });
