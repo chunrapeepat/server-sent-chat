7e0648bf0d07a71bb38756d86b1144d6f7171da4
chunza2542 <hello@thechun.dev>
2021-02-20 11:42:18 +0700

    Handle incoming message

--- frontend/src/ChatRoom.tsx
+++ frontend/src/ChatRoom.tsx
@@ -1,13 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { sendMessage } from "./utils/chat-api";
 
 interface ChatRoomProps {
   username: string;
   endpointURL: string;
 }
+interface MessageEvent {
+  type: "message";
+  username: string;
+  message: string;
+}
 
 function ChatRoom({ username, endpointURL }: ChatRoomProps) {
   const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<any[]>([]);
+
+  useEffect(() => {
+    const events = new EventSource(`${endpointURL}/message`);
+    events.onmessage = (event) => {
+      const data = JSON.parse(event.data) as MessageEvent;
+
+      if (data.username !== username) {
+        setMessages([...messages, data]);
+      }
+    };
+  }, []);
 
   const handleSend = async () => {
     if (!message) {
@@ -18,8 +35,17 @@ function ChatRoom({ username, endpointURL }: ChatRoomProps) {
 
   return (
     <div>
-      <div>Chat message will display here</div>
       <hr />
+      <div>
+        {messages.map((e, i) => {
+          return (
+            <li key={i}>
+              [{e.username}] {e.message}
+            </li>
+          );
+        })}
+      </div>
+
       <span>Message: </span>
       <input type="text" onChange={(e) => setMessage(e.target.value)} />
 
