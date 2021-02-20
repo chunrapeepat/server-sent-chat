0d361d27d6d718ae582ea8d24736d13b25199870
chunza2542 <hello@thechun.dev>
2021-02-20 10:42:33 +0700

    Setup express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world');
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import { PORT } from "./config";
+
+const app = express();
+
+// Setup middlewares
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Define endpoints
+app.get("/", (req, res) => res.send("Hi"));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
